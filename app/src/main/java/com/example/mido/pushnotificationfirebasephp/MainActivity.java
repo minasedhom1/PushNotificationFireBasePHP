2305296b36187b583cb41c7d66d1b13c77b9c7f0
minasedhom1 <mina.raafat1@yahoo.com>
2017-02-14 18:44:12 +0200

    push

--- app/src/main/java/com/example/mido/pushnotificationfirebasephp/MainActivity.java
+++ app/src/main/java/com/example/mido/pushnotificationfirebasephp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+   // http://197.45.12.35/NotifyServices/api/ServiceNotify/?Id=Aya&Token=eJgG_Gpc92g:APA91bFQmYcvzn_c4svt6c08w4aLSggHH8qMlwNOtzFLAqjZvEp3jFY8YLyTdSxhtamXjJ9ovnTmP44lA4AYLNFvpqGmLD4J_GId2i6R4mIB92Qnm0sbMsPh-4BcxPhOG-mmP5_geGFr
 
 
 
